339cf640e23480132de1f969d20e8e6e0928ff0b
Teodor Kolar <tkolar20@foi.hr>
2023-12-28 12:00:37 +0100

    Implemented test producer.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-import fs from "fs";
+import { Kafka } from "kafkajs";
+const kafka = new Kafka(
+{
+    clientId: "test-app",
+    brokers: ["redpanda0:9092"],
+});
+
+const producer = kafka.producer(
+{
+    maxInFlightRequests: 1,
+    idempotent: true,
+    transactionalId: "uniqueProducerId",
+});
+
+async function SendPayload(input: string)
+{
+    try 
+    {
+        await producer.send(
+        {
+            topic: "test",
+            messages: [{key:"test", value:input}],
+        });
+    }
+    catch(e)
+    {
+        console.error("Caught error while sending:" + e);
+    }
+}
+
+await producer.connect();
+await SendPayload("Nekaj tam.");
