77810211e5ede796a7c376ce0e7c6b48e1d1afe9
Teodor Kolar <tkolar20@foi.hr>
2024-01-20 18:58:28 +0100

    TS code cleanup.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import path, { resolve } from "path";
 import produce from "./producer.js";
 import fs from "fs";
 import EventEmitter from "events";
-import PriorityQueue from "./priority_queue.js";
 import axios from "axios";
 import { Stream } from "stream";
 
@@ -11,136 +10,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 const port = 4000;
-//const server = Http.createServer(app);
-//const io = new Server(server);
 
 const url_query = "http://ksqldb-server:8088/query";
 const url_statement = "http://ksqldb-server:8088/ksql";
-
-const eventEmitter = new EventEmitter();
-
-var current = 0;
-var mes = [];
-
-const pq = new PriorityQueue();
-
-/*
-function generateRandomString(length) {
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let randomString = '';
-  
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      randomString += characters.charAt(randomIndex);
-    }
-  
-    return randomString;
-  }
-  function sleep(millis) {
-    return new Promise(resolve => setTimeout(resolve, millis));
-  }
-/*
-
-/*
-io.on('connection', (socket) => {
-    console.log("A client has connected.");
-
-    eventEmitter.on("dataAdded", () =>
-    {
-        const newFrame = pq.dequeue();
-        console.log(newFrame.key);
-        socket.emit("videoFrame", newFrame.message);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Client disconnected');
-    });
-  });
-*/
-
-
-/*app.get("/streaming", async (req, res) => 
-{
-    console.log("streaming");
-    const range = req.headers.range;
-
-    let readStream = new Stream.PassThrough();
-
-    const objs = mes;
-    objs.sort((a, b) => 
-        JSON.parse(a.key) > JSON.parse(b.key)
-        ? 1 
-        : JSON.parse(b.key) > JSON.parse(a.key)
-        ? -1
-        : 0
-    );
-
-    const buf_array = objs.map((b) => b.value)[current];
-    if(!buf_array)
-    {
-        return res.end();
-    }
-
-    const buf = Buffer.from(buf_array);
-
-    current++;
-
-    const start = 0;
-    const end = buf.length;
-    const size = end - start;
-    const head = 
-    {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Range": `bytes ${start}-${end}/17839845`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": size,
-        "Content-Type": "video/mp4",
-    };
-    var myReadableStreamBuffer = new ReadableStreamBuffer(
-    {
-        frequency: 10,
-        chunkSize: 2,
-    });
-    res.writeHead(206, head);
-    readStream.end(buf);
-    readStream.pipe(res);
-});
-
-
-app.get("/file_streaming", (req, res) =>
-{
-    const path = "video/video.mp4";
-    const stat = fs.statSync(path);
-    const fileSize = stat.size;
-    const range = req.headers.range;
-    let start;
-    let end;
-    if(range)
-    {
-        const parts = range.replace(/bytes=/, "").split("-");
-        start = parseInt(parts[0], 10);
-        end = parts[1] ? parseInt(parts[1], 10) : fileSize -1;
-    }
-    else
-    {
-        start = 0;
-        end = 1;
-    }
-
-    const chunksize = end - start + 1;
-    const file = fs.createReadStream(path, {start, end});
-    const head = 
-    {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4",    
-    };
-    res.writeHead(206, head);
-    file.pipe(res);
-});*/
-
 
 app.get("/produce/:id", async (req, res) =>
 {
@@ -168,25 +40,6 @@ app.get("/produce/:id", async (req, res) =>
         {
             console.error(error);
         });
-
-    /*
-    cons.run(
-    {
-        eachMessage: async ({topic, partition, message}) =>
-        {
-            console.log(
-            {
-                value: message.value,
-            });
-            const new_message = Object.assign({}, message);
-            const new_message_key = JSON.parse(message.key.toString());
-            req;
-            const start = 0;
-            const end = message.value.length;
-            const chunksize = end - start + 1;
-            mes.push(new_message);
-        }
-    });*/
     res.setHeader("Content-type", "application/json");
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.send({ message: "producing" });
@@ -196,27 +49,6 @@ app.get("/produce/:id", async (req, res) =>
 app.get("/", async (req, res) =>
 {
     res.sendFile(path.resolve("public/index.html"));
-    /*const id = generateRandomString(5);
-    const consumer = await consume(id);
-    console.log("I consume");*/
-    //await sleep(4000);
-    /*consumer.run(
-    {
-        eachMessage: async ({topic, partition, message}) =>
-        {
-            const frame = message.value;
-            //console.log(message.value.toString());
-            //console.log("I consumed a message");
-            //pq.enqueue({key: parseInt(message.key.toString()), message: message.value});
-            //console.log(message.value.toString());
-            //console.log(message.key.toString());
-            //const newFrame = pq.dequeue();
-            //console.log(newFrame.key);
-            //io.emit("videoFrame", frame);
-            //eventEmitter.emit("dataAdded");
-        }
-    });
-    consumer.seek({ topic: 'test-streaming', partition: 0, offset: "0" });*/
 });
 
 app.get("/video/:id", (req, res) =>
